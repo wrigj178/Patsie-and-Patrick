66e486ae8cb34fe2de414773ee83abe918446c4e
wrigj178 <jjazzymane@live.com>
2014-03-11 14:11:19 -0400

    Initial Commit

---
+++ Patsie and Patrick/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Patsie and Patrick
+//
+//  Created by Jasmine Wright on 3/11/14.
+//  Copyright (c) 2014 Jasmine Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
